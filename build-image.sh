988538dbfb4ae69c6d90e6c0d563d6a1c34919fa
Florian Gebhardt <me@fgardt.dev>
2022-11-24 16:14:47 +0100

    fix(ci): remove branch name tagging

--- build-image.sh
+++ build-image.sh
@@ -9,11 +9,5 @@ IMAGE_NAME="$CI_REGISTRY/se-anwendungsprojekt-22-23/ci-tools/$1"
 # build the commit tag image
 docker build --pull --tag $IMAGE_NAME:$CI_COMMIT_SHA --target $@ $1
 
-# builds in MRs get branch name as tag
-if [ -n "$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME" ]; then
-  docker tag $IMAGE_NAME:$CI_COMMIT_SHA $IMAGE_NAME:$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
-  docker push $IMAGE_NAME:$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
-fi
-
 # push new image tags
 docker push $IMAGE_NAME:$CI_COMMIT_SHA
